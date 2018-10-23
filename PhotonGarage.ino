5fe6fa2fa3eabad38707a72d56517a509b19ba85
positev <44409350+positev@users.noreply.github.com>
2018-10-23 12:06:24 -0500

    Update PhotonGarage.ino

--- PhotonGarage.ino
+++ PhotonGarage.ino
@@ -1,18 +1,34 @@
 /*
  * Project PhotonGarage
- * Description:
- * Author:
- * Date:
+ * Description: by connecting D7 to the trigger pin, 3.3v to vin and gnd to gnd on a relay
+ * Author:Trevor Keegan
+ * Date: 10/23/2018
  */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+int TriggerPin = D7;
 
+void setup() {
+    Particle.function("Garage", OpenFunction);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
+    
+}
+int OpenFunction(String extra){
+  // using a relay that changed position when power is PULLED from the trigger pin uses the following steps
 
+  // prepare to apply power to the pin
+    pinMode(TriggerPin,OUTPUT);
+
+  // apply power to the pin
+    digitalWrite(TriggerPin, LOW);
+
+  // wait 500 milliseconds or half a second
+    delay(500);
+
+  // force the pin to pull power
+    pinMode(TriggerPin,INPUT);
+
+  // return zero because the particle function requires something returned but it realy doesnt matter
+    return 0;
 }
